ef9a8154112ce4932364e091265dc37c9c27a914
Shaswat Das <1999shaswat@gmail.com>
2024-12-20 23:28:08 +0530

    Frontend: remove unused vars

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,17 @@ import Home from "./pages/Home"
 import Room from "./pages/Room"
 import { AppProvider } from "./Context"
 import { useState } from "react"
-import {Toaster} from "sonner"
+import { Toaster } from "sonner"
 
 function App() {
   const [view, setView] = useState<"home" | "room">("home")
   return (
     <AppProvider>
       <div className='bg-gradient-to-b  from-[#FDFCFB] to-[#F7EDE2] min-h-screen'>
-        <Toaster richColors/>
+        <Toaster richColors />
         <div className='h-screen flex items-center justify-center mx-auto max-w-2xl'>
-          {view === "home" && <Home setView={setView}/>}
-          {view === "room" && <Room setView={setView}/>}
+          {view === "home" && <Home setView={setView} />}
+          {view === "room" && <Room />}
         </div>
       </div>
     </AppProvider>
